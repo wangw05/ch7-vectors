868f6f72694d48035b5ebcda38e6cfc4644b3909
Richie Wang <wangw05@uw.edu>
2018-04-05 20:52:21 -0700

    add all responses

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -2,7 +2,7 @@
 
 # Create a vector `words` of 6 (or more) words.
 # You can Google for a "random word generator" if you wish!
-words <- c("Hello", "I", "am", "Richie", "a", "student")
+words <- c("face", "Hello", "I", "am", "Richie", "a", "student")
 
 # Create a vector `words_of_the_day` that is your `words` vector with the string
 # "is the word of the day!" pasted on to the end.
@@ -15,17 +15,20 @@ words_of_the_day <- paste0("'", words, "' is the word of the day!")
 # Hint: use a comparison operator to see if the word comes before "f" alphabetically!
 # Tip: make sure all the words are lower-case, and only consider the first letter
 # of the word!
-a_f_words
+a_f_words <- words[substring(words, 1, 1) <= "f"]
 
 # Create a vector `g_m_words` which are the elements in `words` that start with 
 # "g" through "m"
-
+g_m_words <- words[words >= "g" & words <= "m"]
 
 # Define a function `word_bin` that takes in three arguments: a vector of words, 
 # and two letters. The function should return a vector of words that go between 
 # those letters alphabetically.
-
+word_bin <- function(words, letter1, letter2) {
+  tempvector <- words[words >= letter1 & words <= letter2]
+  tempvector
+}
 
 # Use your `word_bin` function to determine which of your words start with "e" 
 # through "q"
-
+word_bin(words, "e", "w")
